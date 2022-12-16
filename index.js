917397e73a030f4b31d0859bd1a71f4769139287
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2022-12-16 19:48:18 +0000

    debugging

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ var exec = require('child_process').execFile;
 
 
 try
-{
-	const directory = core.getInput("directory")
-	console.log(`Formatting files in ${directory} that end in .nut or .gnut`);
-	
+{	
 	const time = (new Date()).toTimeString();
 	core.setOutput("time", time);
 
+    exec('dir', function(err, data) {  
+        console.log(err)
+        console.log(data.toString());     
+        core.setFailed(err);                  
+    });
     console.log("starting exe");
-    exec('SquirrelStandalone.exe', function(err, data) {  
+    exec('./SquirrelStandalone.exe', function(err, data) {  
         console.log(err)
         console.log(data.toString());     
         core.setFailed(err);                  
